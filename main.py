846ea75529e1d5c59119307649b0a62feccfec06
piekarsky <ptpiekarski@gmail.com>
2022-02-05 23:06:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 import os
 import random
 import argparse
@@ -12,10 +6,16 @@ import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
+
+from models import DNN, RNN, LSTM, GRU, AttentionalLSTM, CNN
+from utils import make_dirs, load_data, plot_full, data_loader, split_sequence_uni_step, split_sequence_multi_step
+from utils import get_lr_scheduler, mean_percentage_error, mean_absolute_percentage_error, plot_pred_test
+
 import torch
 
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
 
-from utils import make_dirs, load_data
 
 
 def main(args):
@@ -32,20 +32,21 @@ def main(args):
     data = load_data(args.which_data)[args.feature]
     data = data.copy()
     
-    
     print(data)
     
-
+        
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--seed', type=int, default=7777, help='seed for reproducibility')
-    parser.add_argument('--which_data', type=str, default='./data/data.xlsx', help='which data to use')
-    parser.add_argument('--weights_path', type=str, default='./results/weights/', help='weights path')
-    parser.add_argument('--plots_path', type=str, default='./results/plots/', help='plots path')
-    parser.add_argument('--numpy_path', type=str, default='./results/numpy/', help='numpy path')
     parser.add_argument('--feature', type=str, default=["lag24", "lag48"], help='extract which feature for prediction')
+    parser.add_argument('--which_data', type=str, default='./data/data.xlsx', help='which data to use')
     
+    parser.add_argument('--multi_step', type=bool, default=False, help='multi-step or not')
+    parser.add_argument('--seq_length', type=int, default=5, help='window size')
+    parser.add_argument('--batch_size', type=int, default=128, help='mini-batch size')
+    
+
     config = parser.parse_args()
     
     
