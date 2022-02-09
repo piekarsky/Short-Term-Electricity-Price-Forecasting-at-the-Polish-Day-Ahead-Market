8e2acb40af80941bf4c187a598490ea826d3e942
piekarsky <ptpiekarski@gmail.com>
2022-02-09 22:12:09 +0100

    add LSTM model

--- models.py
+++ models.py
@@ -23,3 +23,31 @@ class DNN(nn.Module):
         return out
 
 
+class LSTM(nn.Module):
+    """Long Short Term Memory"""
+    def __init__(self, input_size, hidden_size, num_layers, output_size, bidirectional=False):
+        super(LSTM, self).__init__()
+
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.output_size = output_size
+        self.bidirectional = bidirectional
+
+        self.lstm = nn.LSTM(input_size=input_size,
+                            hidden_size=hidden_size,
+                            num_layers=num_layers,
+                            batch_first=True,
+                            bidirectional=bidirectional)
+
+        if self.bidirectional:
+            self.fc = nn.Linear(hidden_size * 2, output_size)
+        else:
+            self.fc = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        out, _ = self.lstm(x)
+        out = out[:, -1, :]
+        out = self.fc(out)
+
+        return out
