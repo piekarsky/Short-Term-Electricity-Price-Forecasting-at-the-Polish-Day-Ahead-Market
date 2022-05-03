8fcda2f038720766d2533e5064db366f2166cc78
piekarsky <ptpiekarski@gmail.com>
2022-05-03 23:07:52 +0200

    Add Vanilla RNN model

--- models.py
+++ models.py
@@ -7,6 +7,44 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
 
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+X, y = next(iter(train_loader))
+
+print("Features shape:", X.shape)
+print("Target shape:", y.shape)
+
+class RNNModel(nn.Module):
+    def __init__(self, num_sensors, hidden_units):
+        super().__init__()
+        self.num_sensors = num_sensors  # this is the number of features
+        self.hidden_units = hidden_units
+        self.num_layers = 1
+
+        self.rnn = nn.RNN(
+            input_size=num_sensors,
+            hidden_size=hidden_units,
+            batch_first=True,
+            num_layers=self.num_layers,
+            dropout=0.6
+            
+        )
+
+        self.linear = nn.Linear(in_features=self.hidden_units, out_features=1)
+
+    def forward(self, x):
+        
+               
+        h0 = torch.zeros(self.num_layers, batch_size, self.hidden_units).requires_grad_()
+        
+        _, hn = self.rnn(x, h0)
+        
+        
+        out = self.linear(hn[0]).flatten()  # First dim of Hn is num_layers, which is set to 1 above.
+
+        return out
 
 
 class LSTMModel(nn.Module):
