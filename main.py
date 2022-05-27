ea2d5d1ef4b2665e1c2cb57c26c23957c2a4bea0
piekarsky <ptpiekarski@gmail.com>
2022-05-27 22:50:50 +0200

    Add the training loop

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import argparse
 import numpy as np
 import pandas as pd
 
-from utils import make_dirs, load_data, standardization, SequenceDataset
+from utils import make_dirs, load_data, standardization, SequenceDataset, train_model, test_model, predict
 import torch
 from sklearn.preprocessing import MinMaxScaler
 from torch.utils.data import DataLoader
@@ -21,7 +21,7 @@ def main(args):
 
     # Prepare Data 
     data = load_data(args.which_data)[args.feature]
-    #print(data)
+    
     
     
     df = data.set_index(['date'])
@@ -81,6 +81,56 @@ def main(args):
     train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
     val_loader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False)
     test_loader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False)
+    
+    
+    
+    
+    
+    model = RNNModel(num_inputs=len(features), hidden_units=args.num_hidden_units)
+    
+
+    loss_function = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    
+    
+    
+    
+    train_plot_losss = []
+    val_plot_losss = []
+
+    for ix_epoch in range(args.num_epochs):
+        print(f"Epoch {ix_epoch}\n---------")
+        train_model(train_loader, model, loss_function, optimizer=optimizer)
+       # train_plot_losss.append(train_model(train_loader, model, loss_function, optimizer=optimizer))
+        test_model(val_loader, model, loss_function)
+  
+        print()
+    
+
+
+    train_eval_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=False)
+
+    ystar_col = "Model forecast"
+    df_train[ystar_col] = predict(train_eval_loader, model).numpy()
+    df_val[ystar_col] = predict(val_loader, model).numpy()
+    df_test[ystar_col] = predict(test_loader, model).numpy()
+
+    df_out = pd.concat((df_train, df_val, df_test))[[target, ystar_col]]
+
+    for c in df_out.columns:
+        df_out[c] = df_out[c] * target_stdev + target_mean
+
+    df_out = df_out.reset_index()
+    df_out = df_out[df_out['date']>='2020-10-01 01:00:00']
+
+
+    result_metrics = calculate_metrics(df_out)
+    
+    return result_metrics
+    
+    
+    
+    
 
 
 if __name__ == "__main__":
@@ -89,13 +139,43 @@ if __name__ == "__main__":
 
     parser.add_argument('--which_data', type=str, default='./data/data.xlsx', help='which data to use')
     parser.add_argument('--seed', type=int, default=7777, help='seed for reproducibility')
-    parser.add_argument('--feature', type=str, default=['value', 'date', 'value lag24', 'value lag48', 'value lag72', 'value lag96', 
-                                                        'value lag120', 'value lag144'], help='extract which feature')
-    
-    parser.add_argument('--output_size', type=int, default=1, help='output_size')
-    
+    parser.add_argument('--output_size', type=int, default=1, help='output_size')   
     parser.add_argument('--seq_length', type=int, default=5, help='window size')
     parser.add_argument('--batch_size', type=int, default=64, help='mini-batch size')
+    parser.add_argument('--feature', type=str, 
+                         default=['value', 'date', 
+                                  'electricity demand',
+                                  'generation of energy from wind sources', 
+                                  'is_weekend',
+                                  'code of the day', 
+                                  'value lag24', 
+                                  'value lag48', 
+                                  'value lag72',
+                                  'value lag96',
+                                  'value lag120', 
+                                  'value lag144', 
+                                  'value lag168',
+                                  'value lag336', 
+                                  'electricity demand lag24', 
+                                  'electricity demand lag48',
+                                  'electricity demand lag72', 
+                                  'electricity demand lag96',
+                                  'electricity demand lag120', 
+                                  'electricity demand lag144',
+                                  'electricity demand lag168', 
+                                  'electricity demand lag336',
+                                  'generation of energy from wind sources lag24',
+                                  'generation of energy from wind sources lag48',
+                                  'generation of energy from wind sources lag72',
+                                  'generation of energy from wind sources lag96',
+                                  'generation of energy from wind sources lag120',
+                                  'generation of energy from wind sources lag144',
+                                  'generation of energy from wind sources lag168',
+                                  'generation of energy from wind sources lag336'], help='extract which feature')
+    
+    parser.add_argument('--lr', type=int, default=0.0001, help='learning rate')
+    parser.add_argument('--num_hidden_units', type=int, default=256, help='hidden units')
+    parser.add_argument('--num_epochs', type=int, default=10, help='num_epochs')
     
     config = parser.parse_args()
 
