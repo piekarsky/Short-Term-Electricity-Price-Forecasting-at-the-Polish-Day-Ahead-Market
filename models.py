b79c04f5ef8b9b2892cae04656e44e0217e146af
piekarsky <ptpiekarski@gmail.com>
2022-06-12 00:29:22 +0200

    Update models

--- models.py
+++ models.py
@@ -1,14 +1,16 @@
 import torch
 import torch.nn as nn
-
 import math
 
 class RNN(nn.Module):
     def __init__(self, num_features, hidden_units, num_layers, output_size, dropout_rate):
         super().__init__()
         self.num_features = num_features  # this is the number of features
+        
+        # Defining the number of layers and the nodes in each layer
         self.hidden_units = hidden_units
         self.num_layers = num_layers
+        
         self.output_size = output_size
         self.dropout = dropout_rate
 
@@ -36,28 +38,34 @@ class RNN(nn.Module):
 
         return out
 
-s
+
 class LSTM(nn.Module):
     def __init__(self, num_features, hidden_units, num_layers, output_size, dropout_rate):
         super().__init__()
         self.num_features = num_features  # this is the number of features
-        self.hidden_units = hidden_units
+        
+         # Defining the number of layers and the nodes in each layer
+        self.hidden_units = hidden_units       
         self.num_layers = num_layers
+        
         self.output_size = output_size
         self.dropout = dropout_rate
 
+        # LSTM layers
         self.lstm = nn.LSTM(
             input_size=num_features,
             hidden_size=hidden_units,
             batch_first=True,
-            num_layers=self.num_layers,
-            dropout=0.6
+            num_layers=num_layers,
+            dropout=dropout_rate
         )
 
         self.linear = nn.Linear(in_features=self.hidden_units, out_features=self.output_size)
 
     def forward(self, x):
         batch_size = x.shape[0]
+        h0 = torch.zeros(self.num_layers, batch_size, self.hidden_units).requires_grad_()
+        
         h0 = torch.zeros(self.num_layers, batch_size, self.hidden_units).requires_grad_()
         c0 = torch.zeros(self.num_layers, batch_size, self.hidden_units).requires_grad_()
 
@@ -68,33 +76,39 @@ class LSTM(nn.Module):
     
     
 class GRU(nn.Module):
-    def __init__(self, input_dim, hidden_dim, layer_dim, output_dim, dropout_prob):
-        super(GRUModel, self).__init__()
-
-        # Defining the number of layers and the nodes in each layer
-        self.layer_dim = layer_dim
-        self.hidden_dim = hiddeddn_dim
+    def __init__(self, num_features, hidden_units, num_layers, output_size, dropout_rate):
+        self.num_features = num_features  # this is the number of features
+        
+         # Defining the number of layers and the nodes in each layer
+        self.hidden_units = hidden_units       
+        self.num_layers = num_layers
+        
+        self.output_size = output_size
+        self.dropout = dropout_rate
 
         # GRU layers
         self.gru = nn.GRU(
-            input_dim, hidden_dim, layer_dim, batch_first=True, dropout=dropout_prob
+            input_size=num_features,
+            hidden_size=hidden_units,
+            batch_first=True,
+            num_layers=num_layers,
+            dropout=dropout_rate
         )
 
         # Fully connected layer
-        self.fc = nn.Linear(hidden_dim, output_dim)
+        self.linear = nn.Linear(hidden_dim, output_dim)
 
     def forward(self, x):
+        
+        batch_size = x.shape[0]
         # Initializing hidden state for first input with zeros
         h0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).requires_grad_()
 
         # Forward propagation by passing in the input and hidden state into the model
-        out, _ = self.gru(x, h0.detach())
-
-        # Reshaping the outputs in the shape of (batch_size, seq_length, hidden_size)
-        # so that it can fit into the fully connected layer
-        out = out[:, -1, :]
-
-        # Convert the final state to our desired output shape (batch_size, output_dim)
-        out = self.fc(out)
-
+        out, _ = self.gru(x, h0.detach())  
+        
+        out = self.linear(hn[0]).flatten()  # First dim of Hn is num_layers, which is set to 1 above.
+      
         return out
+
+   
