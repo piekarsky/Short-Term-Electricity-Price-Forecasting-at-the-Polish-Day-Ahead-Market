a4741fa190b3f41b0ce0229b95c1d6e161e3dd4a
piekarsky <ptpiekarski@gmail.com>
2023-02-25 00:30:28 +0100

    Update models.py

--- models.py
+++ models.py
@@ -14,35 +14,31 @@ class RNN(nn.Module):
         self.output_size = output_size
         self.dropout = dropout_rate
 
+        # RNN layers
         self.rnn = nn.RNN(
             input_size=num_features,
             hidden_size=hidden_units,
             batch_first=True,
             num_layers=num_layers,
-            dropout=dropout_rate
-            
+            dropout=dropout_rate           
         )
 
         self.linear = nn.Linear(in_features=self.hidden_units, out_features=self.output_size)
 
     def forward(self, x):
         
-        batch_size = x.shape[0]
-       
-        h0 = torch.zeros(self.num_layers, batch_size, self.hidden_units).requires_grad_()
-        
+        batch_size = x.shape[0]       
+        h0 = torch.zeros(self.num_layers, batch_size, self.hidden_units).requires_grad_()       
         _, hn = self.rnn(x, h0)
-        
-        
+                
         out = self.linear(hn[0]).flatten()  # First dim of Hn is num_layers, which is set to 1 above.
-
         return out
 
 
 class LSTM(nn.Module):
     def __init__(self, num_features, hidden_units, num_layers, output_size, dropout_rate):
         super().__init__()
-        self.num_features = num_features  # this is the number of features
+        self.num_features = num_features  # the number of features
         
          # Defining the number of layers and the nodes in each layer
         self.hidden_units = hidden_units       
@@ -70,14 +66,14 @@ class LSTM(nn.Module):
         c0 = torch.zeros(self.num_layers, batch_size, self.hidden_units).requires_grad_()
 
         _, (hn, _) = self.lstm(x, (h0, c0))
-        out = self.linear(hn[0]).flatten()  # First dim of Hn is num_layers, which is set to 1 above.
+        out = self.linear(hn[0]).flatten()  # First dim of Hn is num_layers, which is set to 1 above
 
         return out
     
     
 class GRU(nn.Module):
     def __init__(self, num_features, hidden_units, num_layers, output_size, dropout_rate):
-        self.num_features = num_features  # this is the number of features
+        self.num_features = num_features  # the number of features
         
          # Defining the number of layers and the nodes in each layer
         self.hidden_units = hidden_units       
@@ -93,7 +89,7 @@ class GRU(nn.Module):
             num_layers=num_layers,
             dropout=dropout_rate
         )
-
+        
         # Fully connected layer
         self.linear = nn.Linear(in_features=self.hidden_units, out_features=self.output_size)
 
@@ -104,10 +100,8 @@ class GRU(nn.Module):
         h0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).requires_grad_()
 
         # Forward propagation by passing in the input and hidden state into the model
-        out, _ = self.gru(x, h0.detach())  
-        
-        out = self.linear(hn[0]).flatten()  # First dim of Hn is num_layers, which is set to 1 above.
-      
+        out, _ = self.gru(x, h0.detach())         
+        out = self.linear(hn[0]).flatten()  # First dim of Hn is num_layers, which is set to 1 above.      
         return out
 
    
