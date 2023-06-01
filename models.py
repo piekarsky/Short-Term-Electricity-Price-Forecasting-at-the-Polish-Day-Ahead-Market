680e2247c983833a3ba6013e9344c592cbea6c59
piekarsky <ptpiekarski@gmail.com>
2023-06-01 23:38:28 +0200

    Update models.py

--- models.py
+++ models.py
@@ -5,12 +5,11 @@ import math
 class RNN(nn.Module):
     def __init__(self, num_features, hidden_units, num_layers, output_size, dropout_rate):
         super().__init__()
-        self.num_features = num_features  # this is the number of features
+        self.num_features = num_features 
         
         # Defining the number of layers and the nodes in each layer
         self.hidden_units = hidden_units
-        self.num_layers = num_layers
-        
+        self.num_layers = num_layers       
         self.output_size = output_size
         self.dropout = dropout_rate
 
@@ -38,12 +37,11 @@ class RNN(nn.Module):
 class LSTM(nn.Module):
     def __init__(self, num_features, hidden_units, num_layers, output_size, dropout_rate):
         super().__init__()
-        self.num_features = num_features  # the number of features
+        self.num_features = num_features  # 
         
-         # Defining the number of layers and the nodes in each layer
+        # Defining the number of layers and the nodes in each layer
         self.hidden_units = hidden_units       
-        self.num_layers = num_layers
-        
+        self.num_layers = num_layers       
         self.output_size = output_size
         self.dropout = dropout_rate
 
@@ -55,27 +53,24 @@ class LSTM(nn.Module):
             num_layers=num_layers,
             dropout=dropout_rate
         )
-
+        # Fully connected layer
         self.linear = nn.Linear(in_features=self.hidden_units, out_features=self.output_size)
 
     def forward(self, x):
         batch_size = x.shape[0]
         h0 = torch.zeros(self.num_layers, batch_size, self.hidden_units).requires_grad_()
-        
-        h0 = torch.zeros(self.num_layers, batch_size, self.hidden_units).requires_grad_()
         c0 = torch.zeros(self.num_layers, batch_size, self.hidden_units).requires_grad_()
 
         _, (hn, _) = self.lstm(x, (h0, c0))
         out = self.linear(hn[0]).flatten()  # First dim of Hn is num_layers, which is set to 1 above
-
         return out
     
     
 class GRU(nn.Module):
     def __init__(self, num_features, hidden_units, num_layers, output_size, dropout_rate):
-        self.num_features = num_features  # the number of features
+        self.num_features = num_features  
         
-         # Defining the number of layers and the nodes in each layer
+        # Defining the number of layers and the nodes in each layer
         self.hidden_units = hidden_units       
         self.num_layers = num_layers
         self.output_size = output_size
@@ -88,8 +83,7 @@ class GRU(nn.Module):
             batch_first=True,
             num_layers=num_layers,
             dropout=dropout_rate
-        )
-        
+        )       
         # Fully connected layer
         self.linear = nn.Linear(in_features=self.hidden_units, out_features=self.output_size)
 
